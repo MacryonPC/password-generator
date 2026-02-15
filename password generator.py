78147f50ea871b48cde348a283c5cda46e05c2a4
Macryon <philmacro41@gmail.com>
2026-02-15 21:50:09 +0300

    password generator_v1.0.0

---
+++ password generator.py
@@ -0,0 +1,12 @@
+
+import random
+
+chars = '+-/*!&$#?=@<>abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+number = int(input('Кол-во паролей: '))
+length = int(input('Длина пароля: '))  # исправлено: lenght → length
+
+with open('password.txt', 'a') as f:  # эффективная работа с файлом
+    for _ in range(number):
+        password = ''.join(random.choice(chars) for _ in range(length))
+        print(password)
+        f.write(password + '\n')
